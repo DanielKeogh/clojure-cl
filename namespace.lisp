7a1cdefe7365ce9828d2cee6e3eb40cb62162a40
Daniel Keogh <keogh.daniel@gmail.com>
2021-08-27 16:39:52 +1000

    more clojure ns stuff

--- namespace.lisp
+++ namespace.lisp
@@ -11,6 +11,7 @@
 
 (defstruct (clj-namespace (:conc-name ns-))
   (name nil :type symbol)
+  (cl-package nil)
   (aliases (make-hash-table :test 'equal))
   (imports (make-hash-table :test 'equal))
   (interns (make-hash-table :test 'equal))
@@ -19,12 +20,22 @@
   (refers (make-hash-table :test 'equal))
   (macros (make-hash-table)))
 
+;; utils
+
+(defmacro with-namespace ((var sym) &body body)
+  `(alexandria:if-let ((,var (gethash ,sym *namespaces*)))
+     (progn ,@body)
+     (error (format nil "No namespace: ~a found" ,var))))
+
 ;; Creating/changing namespace
 
 (defun create-ns (sym)
   "Create a new namespace named by the symbol if one doesn't already exist, returns it or the already-existing namespace of the same name."
   (or (gethash sym *namespaces*)
-      (setf (gethash sym *namespaces*) (make-clj-namespace :name sym))))
+      (setf (gethash sym *namespaces*) (make-clj-namespace
+					:name sym
+					:cl-package
+					(make-package (symbol-name sym))))))
 
 (defun in-ns (name)
   "Sets *ns* to the namespace named by the symbol, creating it if needed."
@@ -96,8 +107,54 @@ Specs:
   Ret:  any?"
   (error "TODO because macro"))
 
-(defun clj-intern ())
-(defun refer ())
+(defun clj-intern (ns name)
+  (check-type ns symbol)
+  (check-type name symbol)
+  (with-namespace (n ns)
+    (with-accessors ((package ns-cl-package)
+		     (interns ns-interns))
+	n
+      (let ((new-sym (intern (symbol-name name) package)))
+	(unless (gethash new-sym interns)
+	  (setf (gethash new-sym interns) nil))
+	new-sym))))
+
+(defun clj-intern2 (ns name val)
+  (check-type ns symbol)
+  (check-type name symbol)
+  (with-namespace (n ns)
+    (with-accessors ((package ns-cl-package)
+		     (interns ns-interns))
+	n
+      (let ((new-sym (intern (symbol-name name) package)))
+	(setf (gethash new-sym interns) val)
+	new-sym))))
+
+(defun refer (ns-sym &rest filter)
+  "refers to all public vars of ns, subject to filters.
+filters can include at most one each of:
+
+:exclude list-of-symbols
+:only list-of-symbols
+:rename map-of-fromsymbol-tosymbol
+
+For each public interned var in the namespace named by the symbol,
+adds a mapping from the name of the var to the var to the current
+namespace.  Throws an exception if name is already mapped to
+something else in the current namespace. Filters can be used to
+select a subset, via inclusion or exclusion, or to provide a mapping
+to a symbol different from the var's name, in order to prevent
+clashes. Use :use in the ns macro in preference to calling this directly."
+  (with-namespace (n ns-sym)
+    (let ((excludes (getf filter :exclude nil))
+	  (onlies (getf filter :only nil))
+	  (renames (getf filter :rename nil)))
+      (if onlies
+	  (loop for key in (or onlies (alexandria:hash-table-keys (ns-publics n)))
+		for (val s) = (multiple-value-list (gethash key (ns-publics n)))
+		for sym-name = (getf renames key key)
+		unless (find key excludes)
+		  do (setf (gethash sym-name (ns-refers n)) key))))))
 
 ;; Finding namespaces
 
